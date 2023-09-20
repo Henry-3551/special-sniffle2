073b9c27f7a25cff2dd9eddae9570783648d2f05
Chukwuemerie Edmumd <104362482+Henry-3551@users.noreply.github.com>
2023-09-20 11:31:51 +0100

    js.js

---
+++ js.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".type", {
+    strings:  [
+      "Front-end developer", "White Hat Hacker", "Search Engine Expert", "Chess Prodigy", " Proofreader"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  })
+  
+  function doThis() {
+     window.location.href = "https://goolnk.com/JjYp7r";
+  }
